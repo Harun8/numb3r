85ba6bf0f20d774eb499f24f5dd2075276c92531
Harun <harunabdi8@gmail.com>
2023-11-23 18:49:32 +0100

    i called the dbs the same shit ffs

--- utils/db.js
+++ utils/db.js
@@ -7,9 +7,12 @@ export const connectDB = async () => {
       ? process.env.MONGO_URI_PROD
       : process.env.MONGO_URI;
 
+  const dbname =
+    process.env.NODE_ENV === "production" ? "ProdNumb3r" : "Numb3r";
+
   try {
-    await mongoose.connect(mongoURI, {
-      dbName: "Numb3r",
+    await mongoose.connect(process.env.MONGO_URI_PROD, {
+      dbName: dbname,
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
